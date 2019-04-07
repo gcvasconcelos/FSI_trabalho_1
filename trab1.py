f24dbcec533af78eb2dc7b243d0546c1e8ae7cbf
Gabriel Vasconcelosm <gcvasconcelos98@gmail.com>
2019-04-06 22:46:10 -0300

    Mais 4% de acuracia adicionando a media do centro

--- trab1.py
+++ trab1.py
@@ -8,30 +8,42 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 # X contem a matriz (28x28) de escala de cinza da imagem (0 a 255)
 # Y contem a informação de qual é o número (0 a 9)
 # train contem 10.000 imagens e test contem 60.000
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+(pp_x_train, y_train), (pp_x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
+# inicio do preprocessamento
 def split(array, nrows, ncols):
-    r, h = array.shape
-    return (array.reshape(h//nrows, nrows, -1, ncols)
+    return(array.reshape(array.shape[1]//nrows, nrows, -1, ncols)
                 .swapaxes(1, 2)
                 .reshape(-1, nrows, ncols))
 
 # pega a matriz da imagem, divide em n submatrizes, soma o valor total dass submatrizes e armazena no vetor 
-def split_and_sum_array(array, n_of_splits):
-  new_array = np.empty(shape=(array.shape[0],n_of_splits**2))
+def split_and_sum_image(images, n_of_splits):
+  new_images = np.empty(shape=(images.shape[0],n_of_splits**2))
   m = 28 // n_of_splits # precisa ser divisivel por 28 (1, 2, 4, 7, 14, 28)
   i = 0
-  for element in array:
-    subarrays = split(element, m, m)
+  for image in images:
+    subimages = split(image, m, m)
     j = 0
-    for sector in subarrays:
-      new_array[i][j] = sector.sum()
+    for sector in subimages:
+      new_images[i][j] = sector.sum()
       j += 1
+    i += 1
+  return new_images
+
+x_train = split_and_sum_image(pp_x_train, 4)
+x_test = split_and_sum_image(pp_x_test, 4)
+
+def get_center_mean(image, array):
+  new_array = np.empty(shape=(array.shape[0], array.shape[1]+1))
+  i = 0
+  for element in image:
+    center_mean = int(np.mean(element[10:16,10:16]))
+    new_array[i] = np.append(array[i], center_mean)
     i += 1
   return new_array
 
-x_train = split_and_sum_array(x_train, 4)
-x_test = split_and_sum_array(x_test, 4)
+x_train = get_center_mean(pp_x_train, x_train)
+x_test = get_center_mean(pp_x_test, x_test)
 
 # garante que os dados são float e os normaliza
 x_train = x_train.astype('float32')
@@ -61,4 +73,4 @@ plt.figure(figsize = (10,7))
 sn.heatmap(df_cm)
 plt.show()
 
-print('Accuracy ' + str(accuracy_score(y_test, y_pred)))  
+print('Accuracy: ' + str(accuracy_score(y_test, y_pred)))  
